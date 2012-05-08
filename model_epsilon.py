cd31ac7b02637993aac55594b8d24a097d4c1f01
Isaac Tamblyn <isaac.tamblyn@gmail.com>
2012-05-08 15:58:43 -0700

    first working copy

--- model_epsilon.py
+++ model_epsilon.py
@@ -1,9 +1,10 @@
-#!/bin/env python
+#!/usr/bin/env python
 
 import os, sys, commands
 import numpy
 from numpy import log as ln
 from numpy import abs
+
 def model_eps(q,rs,ThomasFermi=True):
 
   kF = 3.63/(rs) # in A^-1, for a free electron gas
@@ -24,12 +25,20 @@ def model_eps(q,rs,ThomasFermi=True):
 
 def main():
 
-  rs = 2.073
+  try:
+    prog = sys.argv[0]
+    rs = float(sys.argv[1])
+  except IndexError:
+    # Tell the user what they need to give
+    print '\nusage: '+prog+' rs (where rs is the density)\n'
+    # Exit the program cleanly
+    sys.exit(0)
+
+  outputFile = open('epsinv.dat','w')
 
   for iq in numpy.arange(0.1,4.0, 0.05):
 
-    print iq, 1/model_eps(iq,rs,True), 1/model_eps(iq,rs,False)
-
+    outputFile.write(str(iq)+ ' ' + str(1/model_eps(iq,rs,True)) + ' ' + str(1/model_eps(iq,rs,False)) + '\n')
 
 # This executes main() only if executed from shell
 if __name__ == '__main__':
